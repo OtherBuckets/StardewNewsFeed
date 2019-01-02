81d04a342a07093a0a7a24439a1061faf9bfbe82
Mike North <mikesnorth@gmail.com>
2019-01-02 11:37:21 -0500

    Adds Mod Entry Point

---
+++ StardewNewsFeed/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace StardewNewsFeed {
+    public class EntryPoint : Mod {
+
+        public override void Entry(IModHelper helper) {
+            throw new NotImplementedException();
+        }
+
+    }
+}
