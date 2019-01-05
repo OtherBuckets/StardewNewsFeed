eea31caa49f1b7f27b7b11029e779728e268d39a
Mike North <mikesnorth@gmail.com>
2019-01-04 20:37:52 -0500

    Properly Name my extension methods

--- StardewNewsFeed/Extensions.cs
+++ StardewNewsFeed/Extensions.cs
@@ -6,14 +6,14 @@ using Netcode;
 namespace StardewNewsFeed {
     public static class Extensions {
 
-        public static string getDisplayName(this GameLocation @this, ITranslationHelper translationHelper) {
+        public static string GetDisplayName(this GameLocation @this, ITranslationHelper translationHelper) {
             if(@this.Name == Constants.FARM_CAVE_LOCATION_NAME) {
                 return translationHelper.Get("news-feed.cave-display-name");
             }
             return @this.Name;
         }
 
-        public static NetBool tileIsReadyForHarvest(this GameLocation @this, int height, int width) {
+        public static NetBool TileIsReadyForHarvest(this GameLocation @this, int height, int width) {
             var objectAtLocation = @this.getObjectAtTile(height, width);
             if(objectAtLocation == null) {
                 return new NetBool(false);
